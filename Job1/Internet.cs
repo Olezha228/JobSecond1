1dd3bb548c6917be8e0c17ae475e9af7793f8237
Olezha228 <87082100+Olezha228@users.noreply.github.com>
2021-09-29 16:43:22 +0500

    Add files via upload

---
+++ Job1/Internet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Job1
+{
+    public class Internet
+    {
+        public static bool IsConnected
+        { 
+            get { return IsConnectedToGoogle() && CanDownloadFile(); } 
+            private set { IsConnected = value; }
+        }
+
+        private static bool IsConnectedToGoogle()
+        {
+            try
+            {
+                string myAddress = "www.google.com";
+                IPAddress[] addresslist = Dns.GetHostAddresses(myAddress);
+
+                if (addresslist[0].ToString().Length > 6)
+                {
+                    return true;
+                }
+                else
+                    return false;
+
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        private static bool CanDownloadFile()
+        {
+            try
+            {
+                WebClient Client = new WebClient();
+                string Response;
+                Response = Client.DownloadString("http://www.google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+         
+    }
+}
---
+++ Job1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Job1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UpdateManager updateManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GetVersion();
+        }
+
+        public void GetVersion()
+        {
+            Assembly assemly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assemly.Location);
+
+            Dispatcher.Invoke(() =>
+            {
+                this.Title = $"v + {fileVersionInfo.FileVersion}";
+                MessageBox.Show($"v + {fileVersionInfo.FileVersion}");
+            });
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            if (Internet.IsConnected)
+            {
+                CheckForUpdates();
+                return;
+            }
+
+            MessageBox.Show("No internet!");
+        }
+
+        private async void CheckForUpdates()
+        {
+            try
+            {
+                using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/Olezha228/JobTask1"))
+                {
+                    updateManager = mgr;
+                    var release = await mgr.UpdateApp();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message + Environment.NewLine;
+                if (ex.InnerException != null)
+                    message += ex.InnerException.Message;
+                MessageBox.Show(message);
+            }
+        }
+    }
+}
